65a04e1058ca19048cd37a79575d683797ad34dd
liangjisheng <1294851990@qq.com>
2019-05-28 17:56:30 +0800

    add token auth

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"os"
 	pb "shippy/consignment-service/proto/consignment"
 	vesselPb "shippy/vessel-service/proto/vessel"
 
+	userPb "shippy/user-service/proto/user"
+
 	"github.com/micro/go-micro"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 )
 
 const (
@@ -29,6 +36,7 @@ func main() {
 		// 必须和 consignment.proto 中的 package 一致
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
+		micro.WrapHandler(AuthWrapper),
 	)
 
 	// 解析命令行参数
@@ -39,5 +47,40 @@ func main() {
 
 	if err := server.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// AuthWrapper 是一个高阶函数，入参是 ”下一步“ 函数，出参是认证函数
+// 在返回的函数内部处理完认证逻辑后，再手动调用 fn() 进行下一步处理
+// token 是从 consignment-ci 上下文中取出的，再调用 user-service 将其做验证
+// 认证通过则 fn() 继续执行，否则报错
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, resp interface{}) error {
+		// consignment-service 独立测试时不进行认证
+		if os.Getenv("DISABLE_AUTH") == "true" {
+			return fn(ctx, req, resp)
+		}
+
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in request")
+		}
+
+		// Note this is now uppercase (not entirely sure why this is...)
+		token := meta["Token"]
+		log.Println("token:", token)
+
+		// Auth here
+		authClient := userPb.NewUserService("go.micro.srv.user", microclient.DefaultClient)
+		authResp, err := authClient.ValidateToken(context.Background(), &userPb.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		log.Println("Auth Resp:", authResp)
+
+		err = fn(ctx, req, resp)
+		return err
 	}
 }

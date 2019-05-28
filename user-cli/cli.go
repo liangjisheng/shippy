36c0fcf7ffac8e1ce9ca8a2ab75c504bbfdbb036
liangjisheng <1294851990@qq.com>
2019-05-28 15:21:09 +0800

    user client test

--- user-cli/cli.go
+++ user-cli/cli.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/micro/cli"
 	"github.com/micro/go-micro"
+	microclient "github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
 
 	pb "shippy/user-service/proto/user"
@@ -47,10 +48,10 @@ func main() {
 			password := c.String("password")
 			company := c.String("company")
 
-			// name = "liangjisheng"
-			// email = "1294851990@qq.com"
-			// password = "liangjisheng"
-			// company = "liangjisheng"
+			name = "liangjisheng"
+			email = "1294851990@qq.com"
+			password = "liangjisheng"
+			company = "liangjisheng"
 
 			r, err := client.Create(context.TODO(), &pb.User{
 				Name:     name,
@@ -71,6 +72,16 @@ func main() {
 				log.Println(v)
 			}
 
+			authResp, err := client.Auth(context.Background(), &pb.User{
+				Email:    email,
+				Password: password,
+			})
+
+			if err != nil {
+				log.Printf("auth failed: %v", err)
+			}
+			log.Println("token:", authResp.Token)
+
 			os.Exit(0)
 		}),
 	)
@@ -79,4 +90,47 @@ func main() {
 	if err := service.Run(); err != nil {
 		log.Println(err)
 	}
+}
+
+func client1() {
+	cmd.Init()
+	// 创建 user-service 微服务的客户端
+	client := pb.NewUserService("go.micro.srv.user", microclient.DefaultClient)
+
+	// 暂时将用户信息写死在代码中
+	name := "Ewan Valentine"
+	email := "ewan.valentine89@gmail.com"
+	password := "test123"
+	company := "BBC"
+
+	resp, err := client.Create(context.TODO(), &pb.User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Company:  company,
+	})
+	if err != nil {
+		log.Fatalf("call Create error: %v", err)
+	}
+	log.Println("created: ", resp.User.Id)
+
+	// allResp, err := client.GetAll(context.Background(), &pb.Request{})
+	// if err != nil {
+	// 	log.Fatalf("call GetAll error: %v", err)
+	// }
+	// for i, u := range allResp.Users {
+	// 	log.Printf("user_%d: %v\n", i, u)
+	// }
+
+	authResp, err := client.Auth(context.TODO(), &pb.User{
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatalf("auth failed: %v", err)
+	}
+	log.Println("token: ", authResp.Token)
+
+	// 直接退出即可
+	os.Exit(0)
 }
